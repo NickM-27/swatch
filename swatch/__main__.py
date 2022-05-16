78c7815b79865e441844008b549511eef30d8923
Nick Mowen <nickmowen213@gmail.com>
2022-05-16 11:16:58 -0600

    Remove other flask log attempt

--- swatch/__main__.py
+++ swatch/__main__.py
@@ -1,14 +1,10 @@
 """Swatch service."""
 
-import sys
 import threading
 
 from swatch.app import SwatchApp
 
 threading.current_thread().name = "swatch"
-
-cli = sys.modules["flask.cli"]
-cli.show_server_banner = lambda *x: None
 
 if __name__ == "__main__":
     swatch_app = SwatchApp()
