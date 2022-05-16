2a79434abd547d56a99eab6433857a9cfc073182
Nick Mowen <nickmowen213@gmail.com>
2022-05-16 11:18:12 -0600

    Fix frontend backend communication

--- web/lib/api/api.dart
+++ web/lib/api/api.dart
@@ -6,7 +6,7 @@ import 'package:swatch/models/config.dart';
 
 class SwatchApi {
   static final SwatchApi _singleton = SwatchApi._internal();
-  static const _swatchHost = "127.0.0.1:4501";
+  static const _swatchHost = "localhost:4500";
 
   factory SwatchApi() {
     return _singleton;
@@ -15,7 +15,7 @@ class SwatchApi {
   SwatchApi._internal();
 
   Future<Config> getConfig() async {
-    const base = "/config";
+    const base = "/api/config";
     final response = await http.get(Uri.http(_swatchHost, base)).timeout(
           const Duration(seconds: 15),
         );
