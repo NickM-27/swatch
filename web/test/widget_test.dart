79c1c2414adb8b1c1da1fcdb23b06328d9f73334
Nick Mowen <nickmowen213@gmail.com>
2022-05-17 12:00:59 -0600

    Add test

--- web/test/widget_test.dart
+++ web/test/widget_test.dart
@@ -9,7 +9,39 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:swatch/main.dart';
+import 'package:swatch/models/config.dart';
 
 void main() {
-  
+  final config = {
+    "objects": {
+      "test_obj": {
+        "color_variants": {
+          "default": {
+            "color_lower": "1, 1, 1",
+            "color_upper": "2, 2, 2",
+          },
+        },
+        "min_area": 0,
+        "max_area": 100000,
+      },
+    },
+    "cameras": {
+      "test_cam": {
+        "snapshot_config": {
+          "url": "http://localhost/snap.jpg",
+        },
+        "zones": {
+          "test_zone": {
+            "coordinates": "1, 2, 3, 4",
+            "objects": ["test_obj"],
+          },
+        },
+      },
+    },
+  };
+
+  test("Config is parsed correctly", () {
+    final swatchConfig = Config(config);
+    assert(swatchConfig.cameras.isNotEmpty);
+  });
 }
