d6dddbc044e99904c2695114500bd3cd80648975
Nick Mowen <nickmowen213@gmail.com>
2022-06-08 14:53:06 -0600

    Improve test

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,13 +11,15 @@ class TestApp(unittest.TestCase):
 
     def setUp(self) -> None:
         """setup simple"""
+        self.db_path = "/media/databases/"
         self.db_file = "/media/databases/swatch.db"
 
     def test_db_created(self) -> None:
+        """Test that the db is created and path is as expected."""
         app = SwatchApp()
         assert not os.path.exists(self.db_file)
         os.environ["DB_FILE"] = self.db_file
         app.__init_db__()
-        assert os.path.exists(self.db_file)
-
+        assert os.path.exists(self.db_path)
+        assert os.path.isfile(self.db_file)
 
